d90f276f451e7420d824a64449f2ecaae0f27026
Bart Massey <bart.massey@gmail.com>
2021-01-03 21:01:41 -0800

    took some rustfmt suggestions

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
+use std::mem::MaybeUninit;
 use std::process::abort;
 use std::ptr;
-use std::mem::MaybeUninit;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 use crossbeam_queue::ArrayQueue;
-use libc::{sigaction, siginfo_t, ucontext_t,
-           c_int, sighandler_t, SA_SIGINFO, SIGINT, pid_t};
+use libc::{
+    c_int, pid_t, sigaction, sighandler_t, siginfo_t, ucontext_t,
+    SA_SIGINFO, SIGINT,
+};
 
-static mut SIGQUEUE: * mut ArrayQueue<pid_t> = ptr::null_mut();
+static mut SIGQUEUE: *mut ArrayQueue<pid_t> = ptr::null_mut();
 static mut MISSES: AtomicUsize = AtomicUsize::new(0);
 
 unsafe fn handle_sigint(
     _signum: c_int,
-    siginfo: * const siginfo_t,
-    _sigcontext: * const ucontext_t,
+    siginfo: *const siginfo_t,
+    _sigcontext: *const ucontext_t,
 ) {
     let pid = siginfo.as_ref().unwrap_or_else(|| abort()).si_pid();
     (*SIGQUEUE).push(pid).unwrap_or_else(|_| {
@@ -29,15 +31,18 @@ fn install_handler() {
         // Set up signal handler.
         // https://stackoverflow.com/a/34377103/364875
         let mut sa: sigaction = MaybeUninit::zeroed().assume_init();
-        sa.sa_sigaction =
-            handle_sigint
-            as *const unsafe fn(c_int, *const siginfo_t, *const ucontext_t)
+        sa.sa_sigaction = handle_sigint
+            as *const unsafe fn(
+                c_int,
+                *const siginfo_t,
+                *const ucontext_t,
+            )
             as sighandler_t;
         sa.sa_flags = SA_SIGINFO;
         let result = sigaction(
             SIGINT,
             &sa as *const sigaction,
-            ptr::null_mut(),
+            ptr::null_mut()
         );
         assert_eq!(result, 0);
     }
