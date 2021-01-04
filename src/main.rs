3da968bf9e3734210fd4e3ecbe0f583d339195cc
Bart Massey <bart.massey@gmail.com>
2021-01-03 20:34:19 -0800

    simplified and cleaned code substantially

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
+use std::ptr;
 use std::mem::MaybeUninit;
 use std::sync::atomic::{AtomicUsize, Ordering::SeqCst};
 
-use lazy_static::lazy_static;
-use parking_lot::ReentrantMutex;
 use crossbeam_queue::ArrayQueue;
 use libc::{sigaction, siginfo_t, ucontext_t,
            c_int, sighandler_t, SA_SIGINFO, SIGINT, pid_t};
 
-lazy_static! {
-    static ref SIGQUEUE: ReentrantMutex<ArrayQueue<pid_t>> =
-        ReentrantMutex::new(ArrayQueue::new(2));
-}
-
+static mut SIGQUEUE: * mut ArrayQueue<pid_t> = ptr::null_mut();
 static mut MISSES: AtomicUsize = AtomicUsize::new(0);
 
 unsafe fn handle_sigint(
@@ -20,14 +15,17 @@ unsafe fn handle_sigint(
     _sigcontext: * const ucontext_t,
 ) {
     let pid = siginfo.as_ref().unwrap().si_pid();
-    let records = SIGQUEUE.lock();
-    records.push(pid).unwrap_or_else(|_| {
+    (*SIGQUEUE).push(pid).unwrap_or_else(|_| {
         let _ = MISSES.fetch_add(1, SeqCst);
     });
 }
 
 fn install_handler() {
     unsafe {
+        // Set up pid queue.
+        SIGQUEUE = Box::leak(Box::new(ArrayQueue::new(2)));
+
+        // Set up signal handler.
         // https://stackoverflow.com/a/34377103/364875
         let mut sa: sigaction = MaybeUninit::zeroed().assume_init();
         sa.sa_sigaction =
@@ -38,7 +36,7 @@ fn install_handler() {
         let result = sigaction(
             SIGINT,
             &sa as *const sigaction,
-            0 as *mut sigaction,
+            ptr::null_mut(),
         );
         assert_eq!(result, 0);
     }
@@ -47,13 +45,11 @@ fn install_handler() {
 
 fn main() {
     install_handler();
-    let records = SIGQUEUE.lock();
-    let mut _count = 0;
     for _ in 0..(1u64 << 63) {
-        if let Some(pid) = records.pop() {
+        let pid = unsafe { (*SIGQUEUE).pop() };
+        if let Some(pid) = pid {
             let misses = unsafe { MISSES.load(SeqCst) };
             println!("{} ({})", pid, misses);
         }
-        _count += 1;
     }
 }
